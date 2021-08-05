4a63dbf6fab4f6260b3bec0133f73e36b46623a0
PerairaCC <carloscoto0103@gmail.com>
2021-08-05 10:06:36 -0600

    Modificacion de modal en la pantalla de Aprobacion de horas

--- js/modalAprobacionHoras.js
+++ js/modalAprobacionHoras.js
@@ -1,43 +1,36 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-var modal2 = document.getElementById("ModalNegacion");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var btn2 = document.getElementById("myBtnNegative");
 
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-var span2 = document.getElementsByClassName("closeN")[0];
+function modalAceptar(nombre) {
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  const modal = document.getElementById("myModal");
+  var btn = document.getElementById(nombre);
+  const span = document.getElementsByClassName("close")[0];
 
-btn2.onclick = function() {
-  modal2.style.display = "block";
-}
+  btn.onclick = () => modal.style.display = "block";
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+  span.onclick = () => modal.style.display = "none";
 
-span2.onclick = function() {
-  modal2.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
   }
 }
 
-window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
+function modalARechazar(nombre) {
+
+  console.log('Model rechazar')
+  const modal2 = document.getElementById("ModalNegacion");
+  const btn2 = document.getElementById(nombre);
+  const span2 = document.getElementsByClassName("closeN")[0];
+
+  btn2.onclick = () => modal2.style.display = "block";
+
+  span2.onclick = () => modal2.style.display = "none";
+
+  window.onclick = function(event) {
+    if (event.target == modal2) {
+      modal2.style.display = "none";
+    }
   }
 }
